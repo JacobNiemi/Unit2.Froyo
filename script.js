1483077044a49744e8978e734a88a51418960306
Jacob Niemi <niemi.m.jacob@gmail.com>
2024-02-20 11:26:00 -0800

    wrote function to count the number of flavors

---
+++ script.js
@@ -0,0 +1,20 @@
+// Function to count the frequency of each flavor in an array
+function countFroyoFlavors(flavors) {
+  const flavorCount = {};
+  flavors.forEach((flavor) => {
+    flavorCount[flavor] = (flavorCount[flavor] || 0) + 1;
+  });
+  return flavorCount;
+}
+
+// Prompt the user for froyo flavors
+const input = prompt(
+  "Please enter your desired froyo flavors seperated by a comma:"
+);
+const flavors = input.split(",");
+
+// Count the frequency of each flavor
+const flavorFrequency = countFroyoFlavors(flavors);
+
+// Display the results in the console
+console.table(flavorFrequency);
